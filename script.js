82ed7d1ef5211a57c20dadb69ce83460e5508a2f
jitendra-jitu <159467483+jitendra-jitu@users.noreply.github.com>
2024-10-22 02:16:15 +0530

    Portofolio Done..

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+function downloadResume() {
+  const link = document.createElement('a');
+  link.href = 'http://localhost/assets/resume.pdf';
+  link.download = 'resume.pdf'; // Specify the default filename
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
