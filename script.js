b07c2e759f512ace702c41f5e1af93adf959ad05
jitendra-jitu <159467483+jitendra-jitu@users.noreply.github.com>
2024-12-10 00:16:05 +0530

    fixed the issues....

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  console.log("menu: ", menu);
+  console.log("icon: ", icon);
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
